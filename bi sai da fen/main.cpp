e7a7f150be4b5f0c22cc71f1f75d8f408552b2d5
20181102932 <908907990@qq.com>
2019-06-20 10:41:27 +0800

    Initial Commit

---
+++ bi sai da fen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bi sai da fen
+//
+//  Created by s20181102932 on 2019/6/20.
+//  Copyright © 2019 s20181102932. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
