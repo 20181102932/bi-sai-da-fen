d60d55ac5e08b3578765072ab64664a87f261852
20181102932 <908907990@qq.com>
2019-06-26 21:10:35 +0800

    just one step
    average is not right
    geshi

--- bi sai da fen/main.cpp
+++ bi sai da fen/main.cpp
@@ -6,7 +6,7 @@ struct student{
     char academy[10];
     char name [10];
     int score[7];
-    int average;
+    double average;
     int rank;
 };
 struct referee{
@@ -43,18 +43,16 @@ int main()
     int e=0;
     int f=0;
     int g=0;
-    int score1[6]={0};
-    int score2[6]={0};
-    int score3[6]={0};
-    int score4[6]={0};
-    int score5[6]={0};
-    int score6[6]={0};
-    int score7[6]={0};
-    int score8[6]={0};
-    int score9[6]={0};
-    int score10[6]={0};
-    int ave=0;
-    int sum=0;
+    int score1[7]={0};
+    int score2[7]={0};
+    int score3[7]={0};
+    int score4[7]={0};
+    int score5[7]={0};
+    int score6[7]={0};
+    int score7[7]={0};
+    int score8[7]={0};
+    int score9[7]={0};
+    int score10[7]={0};
     for(e=0;e<number;e++)
     {
         for(f=0;f<7;f++)
@@ -131,7 +129,261 @@ int main()
             }
         }
     }
+    for(e=0;e<7;e++)
+    {
+        for(f=0;f<7;f++)
+        {
+            if(score1[f]>score1[f+1])
+            {
+                g=score1[f];
+                score1[f]=score1[f+1];
+                score1[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score2[f]>score2[f+1])
+            {
+                g=score2[f];
+                score2[f]=score2[f+1];
+                score2[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score3[f]>score3[f+1])
+            {
+                g=score3[f];
+                score3[f]=score3[f+1];
+                score3[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score4[f]>score4[f+1])
+            {
+                g=score4[f];
+                score4[f]=score4[f+1];
+                score4[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score5[f]>score5[f+1])
+            {
+                g=score5[f];
+                score5[f]=score5[f+1];
+                score5[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score6[f]>score6[f+1])
+            {
+                g=score6[f];
+                score6[f]=score6[f+1];
+                score6[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score7[f]>score7[f+1])
+            {
+                g=score7[f];
+                score7[f]=score7[f+1];
+                score7[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score8[f]>score8[f+1])
+            {
+                g=score8[f];
+                score8[f]=score8[f+1];
+                score8[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score9[f]>score9[f+1])
+            {
+                g=score9[f];
+                score9[f]=score9[f+1];
+                score9[f+1]=g;
+            }
+        }
+    }
+    for(e=0;e<8;e++)
+    {
+        for(f=0;f<8;f++)
+        {
+            if(score10[f]>score10[f+1])
+            {
+                g=score10[f];
+                score10[f]=score10[f+1];
+                score10[f+1]=g;
+            }
+        }
+    }
     
-
+    score1[0]=0;score1[6]=0;
+    score2[0]=0;score2[6]=0;
+    score3[0]=0;score3[6]=0;
+    score4[0]=0;score4[6]=0;
+    score5[0]=0;score5[6]=0;
+    score6[0]=0;score6[6]=0;
+    score7[0]=0;score7[6]=0;
+    score8[0]=0;score8[6]=0;
+    score9[0]=0;score9[6]=0;
+    score10[0]=0;score10[6]=0;
+    double ave[10];
+    double sum[10];
+    sum[0]=score1[1]+score1[2]+score1[3]+score1[4]+score1[5];
+    sum[1]=score2[1]+score2[2]+score2[3]+score2[4]+score2[5];
+    sum[2]=score3[1]+score3[2]+score3[3]+score3[4]+score3[5];
+    sum[3]=score4[1]+score4[2]+score4[3]+score4[4]+score4[5];
+    sum[4]=score5[1]+score5[2]+score5[3]+score5[4]+score5[5];
+    sum[5]=score6[1]+score6[2]+score6[3]+score6[4]+score6[5];
+    sum[6]=score7[1]+score7[2]+score7[3]+score7[4]+score7[5];
+    sum[7]=score8[1]+score8[2]+score8[3]+score8[4]+score8[5];
+    sum[8]=score9[1]+score9[2]+score9[3]+score9[4]+score9[5];
+    sum[9]=score10[1]+score10[2]+score10[3]+score10[4]+score10[5]+score10[6]+score10[0];
+    for(e=0;e<10;e++)
+    {
+        ave[e]=sum[e]/5;
+        student[e].average=ave[e];
+    }
+    for(e=0;e<10;e++)
+    {
+        for(f=0;f<10;f++)
+        {
+            if (ave[f]<ave[f+1])
+            {
+                double z=ave[f];
+                ave[f]=ave[f+1];
+                ave[f+1]=z;
+            }
+        }
+    }
+    ofstream z("/Users/s20181102932/Desktop/比赛结果.txt");
+    g=0;
+    for(e=0;e<number;e++)
+    {
+        for(f=0;f<number;f++)
+        {
+            if(ave[e]==student[f].average)
+            {
+                z<<"第"<<e+1<<"名"<<student[f].name<<"获得"<<ave[e]<<"的平均分"<<endl;
+                if(f==0)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].first<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==1)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].second<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==2)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].third<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==3)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].forth<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==4)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].fifth<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==5)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].sixth<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==6)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].seventh<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==7)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].eighth<<" ";
+                    }
+                    z<<endl;
+                }
+                if(f==8)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<"评委为这位选手打"<<referee[g].ninth<<"分"<<endl<<endl;
+                    }
+                    z<<endl;
+                }
+                if(f==9)
+                {
+                    for(g=0;g<7;g++)
+                    {
+                        z<<referee[g].name<<" "<<referee[g].tenth<<" ";
+                    }
+                    z<<endl;
+                }
+            }
+        }
+    }
+    for(e=0;e<7;e++)
+    {
+        cout<<score1[e]<<endl;
+    }
     return 0;
 }
